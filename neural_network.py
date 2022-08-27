e6d7a3f1e8e8eb39b359dfce32840da8a975be40
Bertin-Johannet Roland <roland.bertin-johannet@etu.umontpellier.fr>
2022-08-27 21:58:13 +0200

    added L2 regularization

--- neural_network.py
+++ neural_network.py
@@ -69,10 +69,8 @@ class Network:
         self.feedforward(input) #retrieve activations
         yhat = self.result
         #compute out layer error
-        #print("errors before : ",[elt.shape for elt in self.errors])
         self.errors[-1] = np.vectorize(sigmoid_prime)(self.linact[-1])   #derivative of activation function
         self.errors[-1] = self.errors[-1] * np.vectorize(cecostprime)(yhat,label) #by the derivative of cost fun
-        #print("errors after : ",[elt.shape for elt in self.errors])
         #compute error for every active neuron
         for i in np.arange(len(self.errors)-2,-1,-1):
             self.errors[i] = self.errors[i] + np.matmul(self.weights[i+1].T,self.errors[i+1])
@@ -89,7 +87,7 @@ class Network:
         correct = sum([int(p==y) for (p,y) in zip(pred,y_transformed)]) 
         return float(correct)/float(len(Y))
 
-    def update_minibatch(self,data,lr):
+    def update_minibatch(self,data,lr,weight_decay=None):
         #initialize gradient with first backprop
         x,y = list(data)[0]
         self.backprop(x,y)
@@ -103,20 +101,25 @@ class Network:
         deltab = [elt/len(list(data)) for elt in deltab]
         deltaw = [elt/len(list(data)) for elt in deltaw]
         #update the weights
-        self.weights = [elt - lr * delta for (delta,elt) in zip(deltaw,self.weights)]
         self.biases = [elt - lr * delta for (delta,elt) in zip(deltab,self.biases)]
+        if weight_decay != None:
+            self.weights = [(1 - (lr*weight_decay)/elt.size) * elt - lr * delta for (delta,elt) in zip(deltaw,self.weights)]
+        else:
+            self.weights = [elt - lr * delta for (delta,elt) in zip(deltaw,self.weights)]
 
 
-    def learn(self,dataX,dataY,lr,epochs,batch_size,testX,testY):
+    def learn(self,dataX,dataY,lr,epochs,batch_size,testX,testY,weight_decay):
         k=0
         data = list(zip(dataX,dataY))
         minibatches = [data[idx:idx+batch_size] for idx in range(0,len(list(data)),batch_size)]
         for i in tqdm.trange(epochs):
             for minibatch,i in zip(minibatches,range(len(minibatches))):
-                self.update_minibatch(minibatch,lr)
+                self.update_minibatch(minibatch,lr,weight_decay)
                 print("minibatch",i )
                 if i%10==0:
-                    print(self.test(testX,testY))
+                    print(self.weights[0].mean(axis=None))
+                    print("test accuracy : ",self.test(testX,testY))
+                    print("train accuracy : ",self.test(dataX[:5000],dataY[:5000]))
 
 nn = Network(784,100,10)
 
@@ -133,4 +136,4 @@ y_test = [[0]*(elt)+[1]+[0]*(9-elt) for elt in y_test]
 #fun = np.vectorize(lambda x:(np.cos(x)+2)/4.)
 #Y_cos = fun(X_cos)
 
-nn.learn(X_train,y_train,0.15,100,512,X_test,y_test)
+nn.learn(X_train,y_train,0.15,100,512,X_test,y_test,5000)
